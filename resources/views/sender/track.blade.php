c1353e678882ed574fe74df08981058d354deb4a
alif <alifianfajarpratama@gmail.com>
2022-03-29 17:31:30 +0700

    Update some stuff

--- resources/views/sender/track.blade.php
+++ resources/views/sender/track.blade.php
@@ -1,6 +1,10 @@
 <ul class="list-group">
     <li class="list-group-item list-group-item-action active">Lokasi Barang</li>
+    @if (count($stops[0]->details) == 0)
+    <li class="list-group-item">Barang Belum Diambil Oleh Kurir!!</li>
+    @else
     @foreach ($stops[0]->details as $index => $stop)
     <li class="list-group-item">{{ $stop->current_location }}</li>
     @endforeach
+    @endif
 </ul>
